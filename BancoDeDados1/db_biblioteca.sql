be7f21d2038d617b386b74fc7ee257acb59c81fe
MateusSilva00 <mateus.2020@alunos.utfpr.edu.br>
2021-08-05 15:02:13 -0300

    Primeiros comando em MySQL

---
+++ BancoDeDados1/db_biblioteca.sql
@@ -0,0 +1,7 @@
+-- CREATE DATABASE db_biblioteca; cria o banco de dados
+-- SHOW DATABASES; mostra todos os bancos de dados na máquina
+-- USE db_biblioca; usa o banco de dados
+-- SELECT DATABASE(); mostra qual banco de dados está sendo utilizado
+-- DROP DATABASE [IF EXISTS] nome_BD; Apaga o banco de dados em questão
+
+SHOW TABLES; -- Mostra as tabelas do banco 
